9c227603234533896a8fb863c882f8b7d61974b2
Hayden Chan <hchan@lcs.on.ca>
2024-05-28 08:36:12 -0400

    Initial Commit

---
+++ TicTacToeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToeApp
+//
+//  Created by Hayden Chan on 2024-05-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToeApp/TicTacToeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAppApp.swift
+//  TicTacToeApp
+//
+//  Created by Hayden Chan on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
