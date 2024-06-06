c06cc49b6d685ca2340c3d04c5920fa1012d05b8
Hayden Chan <hchan@lcs.on.ca>
2024-06-06 11:31:53 -0400

    Made Win fuction

--- TicTacToeApp/Views/GamingScreen.swift
+++ TicTacToeApp/Views/GamingScreen.swift
@@ -16,6 +16,14 @@ enum SquareState {
 struct GamingScreen: View {
     
     // MARK: Stored propeties
+    let winpatterns = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[3,5,7],
+    ]
+    
+
+    
     @State var board = GameBoard()
     
     @State var isItExTurn = Bool.random()
@@ -156,16 +164,85 @@ struct GamingScreen: View {
         }
         
     }
-    let winpatterns = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[3,5,7],
-    ]
     
     func restart(){
         board = GameBoard()
         isItExTurn = Bool.random()
     }
+    func checkWin(forPlayer player: SquareState){
+        if  board.squares[0] == SquareState.ex &&
+            board.squares[1] == SquareState.ex &&
+            board.squares[2] == SquareState.ex ||
+            
+                board.squares[3] == SquareState.ex &&
+                board.squares[4] == SquareState.ex &&
+                board.squares[5] == SquareState.ex ||
+            
+                board.squares[6] == SquareState.ex &&
+                board.squares[7] == SquareState.ex &&
+                board.squares[8] == SquareState.ex ||
+                
+                board.squares[0] == SquareState.ex &&
+                board.squares[3] == SquareState.ex &&
+                board.squares[6] == SquareState.ex ||
+            
+                board.squares[1] == SquareState.ex &&
+                board.squares[4] == SquareState.ex &&
+                board.squares[7] == SquareState.ex ||
+        
+                board.squares[2] == SquareState.ex &&
+                board.squares[5] == SquareState.ex &&
+                board.squares[8] == SquareState.ex ||
+        
+                board.squares[0] == SquareState.ex &&
+                board.squares[4] == SquareState.ex &&
+                board.squares[8] == SquareState.ex ||
+                
+                board.squares[3] == SquareState.ex &&
+                board.squares[5] == SquareState.ex &&
+                board.squares[7] == SquareState.ex  {
+            print("Ex WIns")
+        } else if
+            board.squares[0] == SquareState.oh &&
+                board.squares[1] == SquareState.oh &&
+                board.squares[2] == SquareState.oh ||
+                
+                    board.squares[3] == SquareState.oh &&
+                    board.squares[4] == SquareState.oh &&
+                    board.squares[5] == SquareState.oh ||
+                
+                    board.squares[6] == SquareState.oh &&
+                    board.squares[7] == SquareState.oh &&
+                    board.squares[8] == SquareState.oh ||
+                    
+                    board.squares[0] == SquareState.oh &&
+                    board.squares[3] == SquareState.oh &&
+                    board.squares[6] == SquareState.oh ||
+                
+                    board.squares[1] == SquareState.oh &&
+                    board.squares[4] == SquareState.oh &&
+                    board.squares[7] == SquareState.oh ||
+            
+                    board.squares[2] == SquareState.oh &&
+                    board.squares[5] == SquareState.oh &&
+                    board.squares[8] == SquareState.oh ||
+            
+                    board.squares[0] == SquareState.oh &&
+                    board.squares[4] == SquareState.oh &&
+                    board.squares[8] == SquareState.oh ||
+                    
+                    board.squares[3] == SquareState.oh &&
+                    board.squares[5] == SquareState.oh &&
+                    board.squares[7] == SquareState.oh{
+            print("O wins")
+        }
+       
+                
+    }
+    
+    
+    
+   
 }
 
 #Preview {
