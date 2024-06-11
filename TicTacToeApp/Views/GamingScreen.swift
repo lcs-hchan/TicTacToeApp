f32e8acefeeeb6458a6e0b53014ad362cde99375
Hayden Chan <hchan@lcs.on.ca>
2024-06-11 10:00:45 -0400

    Deleted excess code(win pattern not needed)

--- TicTacToeApp/Views/GamingScreen.swift
+++ TicTacToeApp/Views/GamingScreen.swift
@@ -16,11 +16,6 @@ enum SquareState {
 struct GamingScreen: View {
     
     // MARK: Stored propeties
-    let winpatterns = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6],
-    ]
 
     @State var board = GameBoard()
     
