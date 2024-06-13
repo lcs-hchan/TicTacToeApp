2e3cfd0e90d93af8ef9e9e379d8b50638359be2a
Hayden Chan <hchan@lcs.on.ca>
2024-06-13 12:05:20 -0400

    Finished TIc Tac Toe App by adding Draw Function

--- TicTacToeApp/Views/GamingScreen.swift
+++ TicTacToeApp/Views/GamingScreen.swift
@@ -85,8 +85,6 @@ struct GamingScreen: View {
                                         .font(.system(size: 100))
                                         .ignoresSafeArea()
                                 }
-                               
-                                    
                                 
                             }
                         }
@@ -239,15 +237,7 @@ struct GamingScreen: View {
                     board.squares[6] == SquareState.oh{
             winner = .oh
             oScore += 1
-        } else if board.squares[0] == SquareState.oh &&
-                    board.squares[1] == SquareState.oh &&
-                    board.squares[2] == SquareState.oh &&
-                    board.squares[3] == SquareState.oh &&
-                    board.squares[4] == SquareState.oh &&
-                    board.squares[5] == SquareState.oh &&
-                    board.squares[6] == SquareState.oh &&
-                    board.squares[7] == SquareState.oh &&
-                    board.squares[8] == SquareState.oh
+        } else if board.squares.allSatisfy({$0 != .empty})
                     
         {
         isDraw = true
